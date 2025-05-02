ed9079648b5fd1797718c6c8e459a7a7663b2b14
Matt-Hamilton-GitHub <i.w.b.s.bilm@gmail.com>
2025-05-02 14:26:56 -0700

    main sections added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+
+const Navbar = ({moviesCount}) => {
+
+  const [query, setQuery] = useState("");
+
+  return (
+    <nav className="nav-bar">
+        <div className="logo">
+          <span role="img">🍿</span>
+          <h1>usePopcorn</h1>
+        </div>
+        <input
+          className="search"
+          type="text"
+          placeholder="Search movies..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <p className="num-results">
+          Found <strong>{moviesCount}</strong> results
+        </p>
+      </nav>
+  )
+}
+
+
+export default Navbar
